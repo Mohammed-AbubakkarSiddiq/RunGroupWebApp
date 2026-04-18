39f23787e1f729429e5414586ea2acfada24d35e
Mohammed Abubakkar Siddiq <mohammedabubakkar0701@gmail.com>
2026-04-18 12:54:15 +0530

    Views

--- RunGroupWebApp/Controllers/ClubController.cs
+++ RunGroupWebApp/Controllers/ClubController.cs
@@ -1,12 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RunGroupWebApp.Data;
+using RunGroupWebApp.Models;
 
 namespace RunGroupWebApp.Controllers
 {
     public class ClubController : Controller
     {
+        private readonly ApplicationDbContext _context;
+        public ClubController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            List<Club> clubs = _context.Clubs.ToList();
+            //sends the data to the respective view.
+            return View(clubs);
         }
     }
 }
