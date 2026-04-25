606c29f8f33e2196dac7c9148f3e0b375897df9a
Mohammed Abubakkar Siddiq <mohammedabubakkar0701@gmail.com>
2026-04-25 10:39:01 +0530

    DI, RP, Refactor

---
+++ RunGroupWebApp/Interfaces/IClubRepository.cs
@@ -0,0 +1,15 @@
+﻿using RunGroupWebApp.Models;
+
+namespace RunGroupWebApp.Interfaces
+{
+    public interface IClubRepository
+    {
+        Task<IEnumerable<Club>> GetAllAsync();
+        Task<Club> GetByIdAsync(int id);
+        Task<IEnumerable<Club>> GetByCityAsync(string city);
+        Task<bool> AddAsync(Club club);
+        bool UpdateAsync(Club club);
+        bool DeleteAsync(Club club);
+        Task<bool> SaveAsync();
+    }
+}
