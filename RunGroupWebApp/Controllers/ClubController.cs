c52dd07479945aacb629c000340d1fd154212f45
Mohammed Abubakkar Siddiq <mohammedabubakkar0701@gmail.com>
2026-04-29 12:03:52 +0530

    Create part - 1

--- RunGroupWebApp/Controllers/ClubController.cs
+++ RunGroupWebApp/Controllers/ClubController.cs
@@ -26,5 +26,34 @@ namespace RunGroupWebApp.Controllers
             Club club = await _clubRepository.GetByIdAsync(Id);
             return View(club);
         }
+
+        /// <summary>
+        /// Returns the view for creating a new entity.
+        /// </summary>
+        /// <returns>A view that displays the form for creating a new entity.</returns>
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Handles HTTP POST requests to create a new club entity.
+        /// </summary>
+        /// <remarks>If the provided club model is invalid, the method returns the view with the submitted
+        /// data and validation messages. On successful creation, the user is redirected to the list of clubs.</remarks>
+        /// <param name="club">The club entity to add. Must contain valid data as defined by the model's validation attributes.</param>
+        /// <returns>An IActionResult that redirects to the index view if the club is created successfully; otherwise, returns
+        /// the view with validation errors.</returns>
+        [HttpPost]
+        public async Task<IActionResult> Create(Club club)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(club);
+            }
+
+            await _clubRepository.AddAsync(club);
+            return RedirectToAction("Index");
+        }
     }
 }
