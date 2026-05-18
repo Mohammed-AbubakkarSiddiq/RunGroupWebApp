fe6c32042e3995f368c9ea76dbf2827242c5e41d
Mohammed Abubakkar Siddiq <mohammedabubakkar0701@gmail.com>
2026-05-18 11:33:34 +0530

    Login

---
+++ RunGroupWebApp/Controllers/AccountController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using RunGroupWebApp.Data;
+using RunGroupWebApp.Models;
+using RunGroupWebApp.ViewModels;
+
+namespace RunGroupWebApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public AccountController(ApplicationDbContext context, UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            _context = context;
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel loginVM)
+        {
+            if (!ModelState.IsValid) return View(loginVM);
+
+            var user = await _userManager.FindByEmailAsync(loginVM.EmailAddress);
+
+            if (user != null)
+            {
+                var passwordValid = await _userManager.CheckPasswordAsync(user, loginVM.Password);
+
+                if (passwordValid)
+                {
+                    var result = await _signInManager.PasswordSignInAsync(user, loginVM.Password, false, false);
+
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index", "Race");
+                    }
+                }
+
+                //Not a recommendended way. Using as this is a small app. 
+                //Recommended way: User a property for ths validation in its viewmodel.
+                TempData["Error"] = "Invalid credentials. Please enter valid credentials";
+                return View(loginVM);
+            }
+
+            TempData["Error"] = "Invalid credentials. Please enter valid credentials";
+            return View(loginVM);
+        }
+    }
+}
---
+++ RunGroupWebApp/ViewModels/LoginViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace RunGroupWebApp.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage ="Email address is required.")]
+        [DataType(DataType.EmailAddress)]
+        [DisplayName("Email Address")]
+        public string EmailAddress { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [DisplayName("Password")]
+        public string Password { get; set; }
+    }
+}
