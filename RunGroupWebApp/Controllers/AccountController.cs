70a5e61f5ec8ef4e2bc5000e1af3078aaa22cb5b
Mohammed Abubakkar Siddiq <mohammedabubakkar0701@gmail.com>
2026-05-18 16:00:00 +0530

    Register user

--- RunGroupWebApp/Controllers/AccountController.cs
+++ RunGroupWebApp/Controllers/AccountController.cs
@@ -54,5 +54,51 @@ namespace RunGroupWebApp.Controllers
             TempData["Error"] = "Invalid credentials. Please enter valid credentials";
             return View(loginVM);
         }
+
+        public async Task<IActionResult> Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel registerVM)
+        {
+            if (!ModelState.IsValid) return View(registerVM);
+
+            var user = await _userManager.FindByEmailAsync(registerVM.EmailAddress);
+
+            if (user != null)
+            {
+                TempData["Error"] = "User already exist!";
+                return View(registerVM);
+            }
+
+            var newUser = new AppUser
+            {
+                Email = registerVM.EmailAddress,
+                //Not recomended but it is a simple app. So, just keeping the email and user name same.
+                UserName = registerVM.EmailAddress
+            };
+
+            var userCreationResponse = await _userManager.CreateAsync(newUser, registerVM.Password);
+
+            //This endpoint is meant for user
+            if (userCreationResponse.Succeeded)
+            {
+                await _userManager.AddToRoleAsync(newUser, UserRoles.User);
+                //Not a good practice. But, this is a simple app. Good practice: Redirect to login page.
+                return RedirectToAction("Index", "Race");
+            }
+
+            TempData["Error"] = "Server Error!";
+            return View(registerVM);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Race");
+        }
     }
 }
---
+++ RunGroupWebApp/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace RunGroupWebApp.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [DisplayName("Email Address")]
+        [Required(ErrorMessage ="Email address is required")]
+        [DataType(DataType.EmailAddress)]
+        public string EmailAddress { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DisplayName("Confirm Password")]
+        [Required(ErrorMessage = "Confirm password is required")]
+        [DataType(DataType.Password)]
+        //This will compare the confirm password with the passwork and make sure both of it matches.
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
+    }
+}
